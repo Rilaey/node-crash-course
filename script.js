f405e15e0010adcb01c6f86aff6c467d9169c75d
Riley <94302477+Rilaey@users.noreply.github.com>
2022-11-19 14:14:02 -0500

    learning timeout, interval, and clear

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
-const name = 'ashley';
+// basics of node. operates just like js
 
-console.log(name)
+// const name = 'ashley';
+
+// console.log(name)
+
+// function greet(name) {
+//     console.log('hello, my name is ' + name)
+// }
+
+// greet('riley')
+
+// function meetMe(name, age) {
+//     console.log('Hello, my name is ' + name + ' ! I am ' + age + ' years old.')
+// }
+
+// meetMe('riley', 24)
+// meetMe('ashley', 25)
+
+//////////////////////////////////////////////////////////////////////////////////
+
+// global object
+
+//console.log(global)
+
+// set timeout will run once in 3 seconds
+global.setTimeout(() => {
+    console.log('in the timeout');
+    clearInterval(int) // clearing the int set interval when the timeout of 3 seconds is reached
+}, 3000);
+
+// set interval will run every 1 second
+const int = setInterval(() => {
+    console.log('in the interval')
+}, 1000)
